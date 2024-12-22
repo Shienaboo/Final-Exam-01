061a182fbd5cd1486e084ac4344e10321eaeda21
unknown <nshienarocell@gmail.com>
2024-12-22 01:16:54 -1200

    Inital commit

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+function palindrome() {
+    let racecar= prompt("Enter the word RACECAR");
+    let recorder = prompt("Enter the word RECORDER");
+  
+    const palindrome = (str) => str.split('').reverse().join('') === str;
+  
+    alert("Original word: " + racecar);
+    alert("Original word: " + recorder);
+  
+    alert("Reversed word: " + racecar.split('').reverse().join(''));
+    alert("Reversed word: " + recorder.split('').reverse().join(''));
+  
+    // Check if the strings are palindromes
+    alert(racecar + " is " + palindrome(racecar));
+    alert(recorder + " is " + palindrome(recorder));
+  }
+  
